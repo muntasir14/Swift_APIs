c55a7c24e8e8bc89b36039cfe62b8b5256e221f7
muntasir14 <30627757+muntasir14@users.noreply.github.com>
2019-04-02 20:14:08 +0600

    Works fine for swift 4, iOS 12

---
+++ playingSound.swift
@@ -0,0 +1,26 @@
+//
+//  playingSound.swift
+//  
+//
+//  Created by IKRAMUZZAMAN MUNTASIR on 2/4/19.
+//
+
+import Foundation
+import AVFoundation
+
+// put this code where you want to play a soundfile
+
+var player: AVAudioPlayer?
+
+
+
+guard let url = Bundle.main.url(forResource: "AudioFileName", withExtension: "Extension") else { return }
+
+do {
+    
+    player = try AVAudioPlayer(contentsOf: url!)
+    player.play()
+}catch {
+    
+    // handle error
+}
